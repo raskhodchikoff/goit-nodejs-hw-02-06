201e7de9ba4d5bfa5272f6d7fe7f9c4c2ff3d0ae
raskhodchikoff <artlan@ukr.net>
2023-01-10 15:51:45 +0200

    add auth token

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,7 +7,7 @@ const authenticate = async (req, res, next) => {
   const { authorization = '' } = req.headers
   const [bearer, token] = authorization.split(' ')
   try {
-    if (bearer !== 'Bearer') {
+    if (bearer !== 'Bearer' || !token) {
       return res.status(401).json({ message: 'Not authorized' })
     }
     const { id } = jwt.verify(token, SECRET_KEY)
